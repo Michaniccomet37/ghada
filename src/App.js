84f8578ece35aa8471d49f84c0d0f47b1fbe5e1d
Michaniccomet37 <120781069+Michaniccomet37@users.noreply.github.com>
2026-02-10 00:38:04 +0300

    Create src/App.js with a simple React component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+            <p>Current date and time: 2026-02-09 21:37:45 UTC</p>
+        </div>
+    );
+};
+
+export default App;
